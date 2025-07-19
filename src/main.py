051790797e8091e4426e5c20d64ea13264397d96
Xyno-dev <zayanarshad88@gmail.com>
2025-07-19 21:43:07 +0100

    feat: device class

--- src/main.py
+++ src/main.py
@@ -8,74 +8,69 @@ import os
 
 load_dotenv()
 
-url = "http://100.64.0.1:8123/api/states"
-headers = {
-    "Authorization": f"Bearer {os.getenv("token")}",
-    "content-type": "application/json",
-}
+class Device():
+    def __init__(self, entity_id: str, domain: str, headers: dict):
+        self.id = entity_id
+        self.domain = domain
+        self.headers = headers
 
-jsonresponse = get(url, headers=headers)
-response = json.loads(jsonresponse.text)
-'''
-for device in response:
-    pprint.pp(device["entity_id"])
-'''
+    def run_service(self, service):
+        jsonresponse = post(
+            f"http://100.64.0.1:8123/api/services/{self.domain}/{service}",
+            headers=self.headers,
+            json={"entity_id": self.id}
+        )
+        response = jsonresponse.json()
+        pprint.pp(response)
 
-def load_device(data):
+def load_device(data, headers) -> list:
     devices = []
     try:
-        for devtype in data:
-            for device in data[devtype]:
-                if data[devtype][device]['enabled']:
-                    devices.append(devtype + "." + device)
+        devices = [
+            Device(f"{domain}.{device}", domain, headers)
+            for domain in data
+            for device in data[domain]
+            if data[domain][device]['enabled']
+        ]
     except KeyError:
         raise tomllib.TOMLDecodeError("toml syntax is poo")
     return devices
 
-def device_valid(device, response):
+def device_valid(device, response) -> bool:
     devices = []
     for resdevice in response:
         devices.append(resdevice['entity_id'])
-    if device in devices:
+    if device.id in devices:
         return True
     else:
         return False
 
-def turn_on(device):
-    print(device, device.split(".")[0])
-    jsonresponse = post(
-        f"http://100.64.0.1:8123/api/services/{device.split(".")[0]}/turn_on",
-        headers=headers,
-        json={"entity_id": device}
-    )
-    response = jsonresponse.json()
-    pprint.pp(response)
-
-def turn_off(device):
-    print(device, device.split(".")[0])
-    jsonresponse = post(
-        f"http://100.64.0.1:8123/api/services/{device.split(".")[0]}/turn_off",
-        headers=headers,
-        json={"entity_id": device}
-    )
-    response = jsonresponse.json()
-    pprint.pp(response)
-        
 def main():
+    devices = []
+
+    url = "http://100.64.0.1:8123/api/states"
+    headers = {
+        "Authorization": f"Bearer {os.getenv("token")}",
+        "content-type": "application/json",
+    }
+
+    jsonresponse = get(url, headers=headers)
+    response = json.loads(jsonresponse.text)
+
     try:
         with open("neohome.toml", "rb") as f:
             data = tomllib.load(f)
-            devices = load_device(data)
-            while True:
-                for device in devices:
-                    if device_valid(device, response):
-                            turn_off(device)
-                            time.sleep(1)
-                            turn_on(device)
-                            time.sleep(1)
-
+            devices = load_device(data, headers)
     except FileNotFoundError:
         raise FileNotFoundError("neohome.toml not found")
+           
+    while True:
+        for device in devices:
+            if device_valid(device, response):
+                device.run_service("turn_off")
+                time.sleep(1)
+                device.run_service("turn_on")
+                time.sleep(1)
 
 if __name__ == "__main__":
     main()
